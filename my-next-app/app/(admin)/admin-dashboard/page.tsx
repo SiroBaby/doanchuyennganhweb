bcfe5bb83820976474df1d3e9fabd0b4e1a590e1
SiroBaby <ngocphat076@gmail.com>
2024-10-08 16:11:55 +0700

    optimize transition

--- my-next-app/app/(admin)/admin-dashboard/page.tsx
+++ my-next-app/app/(admin)/admin-dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-        <div className='col-span-2 dark:bg-dark-body dark:text-dark-text h-screen'>Đây là admin</div>
+        <div className='col-span-2 dark:bg-dark-body dark:text-dark-text h-screen transition-colors duration-200'>Đây là admin</div>
   )
 }
 
--- my-next-app/app/component/layout/LeftSideBar.tsx
+++ my-next-app/app/component/layout/LeftSideBar.tsx
@@ -8,14 +8,14 @@ import { SvgIcon } from "@mui/material";
 //h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-slate-400 shadow-xl
 const LeftSideBar = () => {
   return (
-    <div className="h-screen w-60 left-0 top-0 sticky flex flex-col bg-white shadow-xl dark:bg-dark-sidebar">
+    <div className="h-screen w-60 left-0 top-0 sticky flex flex-col bg-white shadow-xl dark:bg-dark-sidebar transition-colors duration-200">
       <div className="h-32 flex justify-center">
         <Image src="/logo/logo.png" width={120} height={0} alt="logo" />
       </div>
       <div className="left-0 dark:text-dark-text">
         <ul>
           <li className="border-b border-outline dark:border-dark-outline"></li>
-          <li className="flex p-4 text-2xl font-semibold  bg-selected-corlor dark:bg-dark-selected pl-9 items-center cursor-pointer border-r-8 border-custom-red">
+          <li className="flex p-4 text-2xl font-semibold  bg-selected-corlor dark:bg-dark-selected transition-colors duration-200 pl-9 items-center cursor-pointer border-r-8 border-custom-red">
             <SvgIcon component={DashboardIcon} className=" h-auto w-8 text-custom-red"/>
             <span className="ml-2 text-2xl font-bold">Dashboard</span>
           </li>
