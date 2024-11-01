f9c951f8275374b70dfa8f430c96457186a6fe0b
SiroBaby <ngocphat076@gmail.com>
2024-11-01 13:02:16 +0700

    run build

--- my-next-app/app/store/store.tsx
+++ my-next-app/app/store/store.tsx
@@ -1,18 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { usersApi } from "./api/users";
 import { vehiclesApi } from "./api/vehicleapi";
 import darkModeReducer from "./slices/darkModeSlices";
 
 export const store = configureStore({
   reducer: {
-    [usersApi.reducerPath]: usersApi.reducer,
     [vehiclesApi.reducerPath]: vehiclesApi.reducer,
     darkMode: darkModeReducer,
   },
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
-      .concat(usersApi.middleware)
       .concat(vehiclesApi.middleware)
 });
 
