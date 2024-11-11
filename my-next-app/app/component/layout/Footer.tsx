5621d299443c9b30cf313bcea2319d587e32d6b8
huyen35 <hyhy91177@gmail.com>
2024-11-11 22:58:15 +0700

    footer tạm thời

--- my-next-app/app/component/layout/Footer.tsx
+++ my-next-app/app/component/layout/Footer.tsx
@@ -9,9 +9,7 @@ const Footer = () => {
   return (
     <div className="w-full bg-custom-blue py-8 px-4 md:px-12">
       <div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
-        {/* Hàng 1 */}
         <div className="flex justify-between items-start">
-          {/* Logo */}
           <div className="w-40 h-40 relative">
             <Image
               src="/logo/logo.png"
@@ -22,7 +20,6 @@ const Footer = () => {
             />
           </div>
 
-          {/* Du lịch trong nước */}
           <div className="space-y-4">
             <h3 className="font-bold text-lg">DU LỊCH TRONG NƯỚC</h3>
             <ul className="space-y-2">
@@ -45,14 +42,14 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* Hàng 2 */}
         <div className="flex justify-between items-start">
-          {/* Liên hệ */}
           <div className="space-y-4">
             <h3 className="font-bold text-lg">LIÊN HỆ</h3>
             <div className="space-y-2">
               <p>SĐT: 0123456789</p>
-              <p>Địa chỉ: 18A/1 Cộng Hòa, phường 12, Tân Bình, TP.HCM</p>
+              <a href="https://www.google.com/maps/place/H%E1%BB%8Dc+Vi%E1%BB%87n+H%C3%A0ng+Kh%C3%B4ng+Vi%E1%BB%87t+Nam+CS2/@10.7999506,106.6533982,18.59z/data=!4m6!3m5!1s0x3175292976c117ad:0x5b3f38b21051f84!8m2!3d10.8000211!4d106.6544162!16s%2Fg%2F1td1mcc1?hl=vi-VN&entry=ttu&g_ep=EgoyMDI0MTEwNi4wIKXMDSoASAFQAw%3D%3D">
+                <p>Địa chỉ: 18A/1 Cộng Hòa, phường 12, Tân Bình, TP.HCM</p>
+              </a>
               <div className="flex items-center space-x-4">
                 <a href="https://www.facebook.com/phat.huynhngoc.3" target="_blank" rel="noopener noreferrer">
                   <FacebookIcon className="text-2xl" />
@@ -64,7 +61,6 @@ const Footer = () => {
             </div>
           </div>
 
-          {/* Thông tin */}
           <div className="space-y-4">
             <h3 className="font-bold text-lg">THÔNG TIN</h3>
             <ul className="space-y-2">
@@ -76,7 +72,6 @@ const Footer = () => {
         </div>
       </div>
 
-      {/* Copyright text */}
       <div className="text-center mt-8 text-sm text-gray-600 border-t pt-4">
         <p>Đồ án chuyên ngành - Học kì I - 2024-2025</p>
       </div>
